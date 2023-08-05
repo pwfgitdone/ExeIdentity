9db84b76c862f1de976dc509062b64da35e53e4e
Daniel Cardoso Carneiro <ddrdan7@hotmail.com>
2023-08-05 18:50:07 -0300

    Initial

---
+++ TesteIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
